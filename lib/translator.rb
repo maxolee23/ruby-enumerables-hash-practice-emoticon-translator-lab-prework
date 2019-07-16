5551f08f6ae6d029fa2b4cd29e8eaf9d1f868c62
Max Lee <maxolee23@gmail.com>
2019-07-16 17:51:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,14 @@ require 'pry'
 require 'yaml'
 
 def load_library(file_path)
-  # code goes here
-end
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
+end 
 
 def get_japanese_emoticon
   # code goes here
